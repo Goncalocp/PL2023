6ae34985850cb7695bf334448e1250a0d0739d90
Goncalocp <goncalocp02@gmail.com>
2023-03-03 18:33:29 +0000

    Update tpc3.py

--- TPC3/tpc3.py
+++ TPC3/tpc3.py
@@ -34,9 +34,20 @@ def sortDic(dic):
     return sorted_dic
 
 def sortDicByValue(dic):
-    sorted_dic = sorted(dic.items(), key=lambda kv:kv[1])
+    sorted_dic = dict( sorted(dic.items(),key=lambda item: item[1],reverse=True))
 
     return sorted_dic
+
+def getFirst(num, dic):
+    newDic = {}
+    i = 0
+    for key in dic:
+        update(newDic,key,dic[key])
+        i += 1
+        if i >= num:
+            break
+
+    return newDic
 
 def procByYear(data):
     result = dict()
@@ -57,6 +68,7 @@ def namesFreq(data):
     for i in data:
         first_names = re.findall(r'::([A-Z][a-z][a-z]+)\s+[A-Z]',i.get('names'))
         last_names = re.findall(r'[a-z]+\s([A-Z][a-z][a-z]+)[:|,]+',i.get('names'))
+        last_names2 = re.findall(r'[a-z]+\s([A-Z][a-z][a-z]+)::',i.get('names'))
         date = i['date'][:4]
         if date[2:4] == '00':
             x = int(date[0:2])
@@ -74,6 +86,10 @@ def namesFreq(data):
         if last_names != []:
             for k in last_names:
                 dicLastNames[x] = update(dicLastNames[x],k,1)
+
+        if last_names2 != []:
+            for k in last_names2:
+               dicLastNames[x] = update(dicLastNames[x],k,1)
     
     for key in dicFirstNames:
         dicFirstNames[key] = sortDicByValue(dicFirstNames[key])
@@ -110,6 +126,7 @@ def json(data):
             else:
                 fileContent += f'        {key}: {dic[key]}\n'
         if i<19: fileContent += "    },\n"
+        else: fileContent += "    }\n"
     fileContent += "]\n"
     file.write(fileContent)
     file.close()
@@ -134,20 +151,37 @@ def namesFraqTable(result):
 
     print()
     print(" >  Frequência de nomes próprios e apelidos por século  < \n")
-    print("+------------------------------+")
-    print("|  Século  |        Nome       |")
-    print("+------------------------------+")
-    print("+------------------------------+")
+    print("+-------------------------------+")
+    print("|  Século  |        Nome        |")
+    print("+-------------------------------+")
+    print("+-------------------------------+")
 
     for key in first_names:
-        dic = first_names[key]
-        for i in range(1,6):
-            name = dic[i]
+        dic = getFirst(5,first_names[key])
+        i = 1
+        for name in dic:
             if i == 3:
-                print("|  {:^2}   |{:^12} ({:^3})|".format(key,name[0],name[1]))
+                print("|    {:^2}    |{:^12}({:^6})|".format(key,name,dic[name]))
             else:
-                print("|          |{:^12} ({:^3})|".format(name[0],name[1]))
-        print("+------------------------------+")
+                print("|          |{:^12}({:^6})|".format(name,dic[name]))
+            i += 1
+        print("+-------------------------------+")
+
+    print("+-------------------------------+")
+    print("|  Século  |       Apelido      |")
+    print("+-------------------------------+")
+    print("+-------------------------------+")
+
+    for key in last_names:
+        dic = getFirst(5,last_names[key])
+        i = 1
+        for name in dic:
+            if i == 3:
+                print("|    {:^2}    |{:^12}({:^6})|".format(key,name,dic[name]))
+            else:
+                print("|          |{:^12}({:^6})|".format(name,dic[name]))
+            i += 1
+        print("+-------------------------------+")
 
 
 
@@ -166,12 +200,12 @@ def relationsFreqTable(result):
 
 def main():
     data = parser("processos.txt")
-    #result1 = procByYear(data)
-    #procByYearTable(result1)
+    result1 = procByYear(data)
+    procByYearTable(result1)
     namesFraqTable(namesFreq(data))
-    #result3 = relationsFreq(data)
-    #relationsFreqTable(result3)
-    #json(data)
+    result3 = relationsFreq(data)
+    relationsFreqTable(result3)
+    json(data)
     
 
 if __name__ == "__main__":
